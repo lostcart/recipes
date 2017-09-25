3bd8d8dae7ea0f056bc7582ec1d4bf3d0082ba78
Luke Tedman <luketedman@gmail.com>
2017-09-25 14:52:25 +0100

    Pull down recipes and log them out

--- app/src/main/java/lost/cart/recipes/features/recipes/RecipesActivity.java
+++ app/src/main/java/lost/cart/recipes/features/recipes/RecipesActivity.java
@@ -3,6 +3,7 @@ package lost.cart.recipes.features.recipes;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.jakewharton.rxbinding2.widget.RxTextView;
 
@@ -12,8 +13,10 @@ import butterknife.Bind;
 import io.reactivex.Observable;
 import lost.cart.recipes.R;
 import lost.cart.recipes.data.local.models.Recipe;
+import lost.cart.recipes.data.remote.recipes.RecipesHelper;
 import lost.cart.recipes.features.base.BaseActivity;
 import test.base.lost.com.baselibrary.LostBasePresenter;
+import timber.log.Timber;
 
 public class RecipesActivity extends BaseActivity implements RecipesPresenter.View {
     @Bind(R.id.recipes_edittext_search) EditText searchEditText;
@@ -29,7 +32,7 @@ public class RecipesActivity extends BaseActivity implements RecipesPresenter.Vi
     @NonNull
     @Override
     protected LostBasePresenter initialisePresenter() {
-        return new RecipesPresenter();
+        return new RecipesPresenter(new RecipesHelper());
     }
     @Override
     protected void initialiseView() {
@@ -41,9 +44,11 @@ public class RecipesActivity extends BaseActivity implements RecipesPresenter.Vi
 
     @Override
     public void showRecipes(List<Recipe> recipes) {
+        Timber.d(recipes.toString());
     }
 
     @Override
     public void showError(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/lost/cart/recipes/features/recipes/RecipesPresenter.java
+++ app/src/main/java/lost/cart/recipes/features/recipes/RecipesPresenter.java
@@ -6,7 +6,11 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import lost.cart.recipes.data.local.models.Recipe;
+import lost.cart.recipes.data.remote.recipes.RecipesHelper;
 import lost.cart.recipes.features.base.BasePresenter;
 import timber.log.Timber;
 
@@ -16,18 +20,46 @@ import timber.log.Timber;
 public class RecipesPresenter extends BasePresenter<RecipesPresenter.View> {
     private static final long WAIT_TIME_BEFORE_SEARCH_MS = 500;
 
+    private RecipesHelper recipesHelper;
+    private Disposable disposable;
+
+    public RecipesPresenter(RecipesHelper recipesHelper) {
+        this.recipesHelper = recipesHelper;
+    }
+
     @Override
     public void onViewAttached(@NonNull View view) {
         super.onViewAttached(view);
 
+        // Listen for changes to the search term and perform a search once the user
+        // has stopped making changes for some time
         unsubscribeOnDetach(view.onSearchTermChangedObservable()
                 .debounce(WAIT_TIME_BEFORE_SEARCH_MS, TimeUnit.MILLISECONDS).subscribe(
-                success -> Timber.d("text changed"),
-                error -> Timber.e("Something went wrong with text change")));
+                        this::getRecipes,
+                        error -> Timber.e("Something went wrong with text change")));
     }
 
-    private void getRecipes(CharSequence searchTerm){
+    private void getRecipes(CharSequence searchTerm) {
+        String searchString = searchTerm.toString();
 
+        // Discard any previous searches that are happening
+        if (disposable != null && !disposable.isDisposed()) {
+            disposable.dispose();
+        }
+
+        // Make a search with the given searchTerm
+        disposable = recipesHelper.getRecipes(searchString)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        results -> getView().showRecipes(results),
+                        error -> {
+                            String errorMessage = error.getLocalizedMessage();
+                            getView().showError(errorMessage);
+                            Timber.e("Error getting recipes :" + errorMessage);
+                        }
+                );
+        unsubscribeOnDetach(disposable);
     }
 
     public interface View extends BasePresenter.View {
